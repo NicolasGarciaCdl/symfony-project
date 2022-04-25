236a2945aaa7104416815f2da18eefd130ec82fb
NicolasGarciaCdl <NicolasGarciaCdl@users.noreply.github.com>
2022-04-25 11:54:26 +0200

    update LuckyController.php

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    /**
+     * @Route("lucky/number")
+     * @return Response
+     * @throws Exception
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html lang="en"><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
